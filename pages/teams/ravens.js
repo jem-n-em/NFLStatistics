c360efb2b7a6478ac2ee3fbc0ede884f2c25a2f6
Jerami Soriano <jerami4122@yahoo.com>
2024-06-14 01:43:08 -0400

    initial commit

---
+++ pages/teams/ravens.js
@@ -0,0 +1,19 @@
+const data_ravens = {
+    name: "Baltimore Ravens",
+    location: "Baltimore, MD",
+    stadium: "M&T Bank Stadium",
+    established: "1996",
+
+    personnel: {
+        headCoach: "John Harbaugh",
+
+    }
+}
+
+var teamData = data_ravens;
+const teamMarkup = (teamData) => {
+    return '
+    <div>
+        
+    ';
+}
